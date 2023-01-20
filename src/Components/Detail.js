5e890b1c563dcc4be3a5bdb41401960b8ef5b548
edmund <edmund1000@naver.com>
2023-01-20 18:29:55 +0900

    줄간격, 굵기 수정

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -4,14 +4,12 @@ import { Badge, Divider, Space } from 'antd';
 import { Tag } from 'antd';
 import styled from 'styled-components';
 
-  const Detail = () => {
-    
+const Detail = () => {
     const colors = [
-
         'year 과목년도',
         'semester 학기',
         'grade 수강 학년',
-        'college 수업 소속 대학 V',
+        'college 수업 소속 대학 ',
         'major 학과(학부) ',
         'credit 학점',
         'lecture_credit 이론 인정 학점',
@@ -24,85 +22,121 @@ import styled from 'styled-components';
         'total 총원',
         'sugang 수강 신청 한 인원',
         'sugangpack 수강 꾸러미 담은 인원',
+    ];
 
-    ]
+    return (
+        <>
+            <YellowBack>
+                <Wrap className="site-card-border-less-wrapper">
+                    <Card bordered={false}>
+                        <Header>
+                            <h1>동역학</h1>
+                            <h5>CRTR23423</h5>
+                        </Header>
 
+                      <DividerWrap>
+                        <Divider orientation="left">
+                            {' '}
+                            <CarryOutOutlined /> 인기도
+                        </Divider>
+                      </DividerWrap>
 
-  return (
+                        <Popularity>
+                            <div className="site-card-wrapper">
+                                <Row gutter={16}>
+                                    <Col span={8}>
+                                        <Card
+                                            title="Sugangpack/sugang"
+                                            bordered={false}
+                                        >
+                                            150%
+                                        </Card>
+                                    </Col>
 
-    
-<>  
-  <YellowBack>
-  <Wrap className="site-card-border-less-wrapper"
-  >
+                                    <Col span={8}>
+                                        <Card
+                                            title="Sugangpack/total"
+                                            bordered={false}
+                                        >
+                                            190%
+                                        </Card>
+                                    </Col>
 
-    <Card bordered={false}>
+                                    <Col span={8}>
+                                        <Card title="favor" bordered={false}>
+                                            30%
+                                        </Card>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </Popularity>
 
-      <Header>
-        <h1>동역학</h1>
-        <h5>CRTR23423</h5>
-        
-      </Header>
-
-      
-      
-      
-
-  <Divider orientation="left"> <CarryOutOutlined /> 인기도</Divider>
-
-  <Popularity>
-  <div className="site-card-wrapper">
-    <Row gutter={16}>
-
-      <Col span={8}>
-        <Card title="Sugangpack/sugang" bordered={false}>
-          150%
-        </Card>
-      </Col>
-
-      <Col span={8}>
-        <Card title="Sugangpack/total" bordered={false}>
-          190%
-        </Card>
-      </Col>
-
-      <Col span={8}>
-        <Card title="favor" bordered={false}>
-          30%
-        </Card>
-      </Col>
-
-    </Row>
-  </div>
-  </Popularity>
-  
-
-  <Divider orientation="left" > <CarryOutOutlined />   기본정보</Divider>
-        <HashTag>
-      <Tag color="magenta" >교수님</Tag>
-      <Tag color="red">학부(대학)</Tag>
-      <Tag color="volcano">type</Tag>
-      <Tag color="orange">information</Tag>
-      {/* <Tag color="gold">gold</Tag>
+                        <Divider orientation="left">
+                            {' '}
+                            <CarryOutOutlined /> 기본정보
+                        </Divider>
+                        <HashTag>
+                            <Tag color="magenta">교수님</Tag>
+                            <Tag color="red">학부(대학)</Tag>
+                            <Tag color="volcano">type</Tag>
+                            <Tag color="orange">information</Tag>
+                            {/* <Tag color="gold">gold</Tag>
       <Tag color="lime">lime</Tag>
       <Tag color="green">green</Tag>
       <Tag color="cyan">cyan</Tag>
       <Tag color="blue">blue</Tag>
       <Tag color="geekblue">geekblue</Tag>
       <Tag color="purple">purple</Tag> */}
-    </HashTag>
-    
-    <Divider orientation="left"> <CarryOutOutlined />   세부 수업 관련 정보</Divider>
-    
-    <Contents>
-      <Space direction="vertical">
-        {colors.map((colors) => (
-          <Badge key={colors} color={colors} text={colors} />
-        ))}
-      </Space>
-    </Contents>
-    
-        {/* <Row style={{padding : 60}}>
+                        </HashTag>
+
+                        <Divider orientation="left">
+                            {' '}
+                            <CarryOutOutlined /> 세부 수업 관련 정보
+                        </Divider>
+
+                        <Contents>
+                            <Space direction="vertical" level={5}>
+                                {colors.map((colors) => (
+                                    <Badge
+                                        key={colors}
+                                        color={colors}
+                                        text={colors}
+                                      
+                                    />
+                                ))}
+                            </Space>
+                        </Contents>
+
+                        <Divider orientation="left">
+                            <CommentOutlined /> 리뷰
+                        </Divider>
+
+          <Popularity>
+                <div className="site-card-wrapper">
+                  <Row gutter={16}>
+
+                    <Col span={8}>
+                      <Card title="review_number(수강평가한 인원)" bordered={false}>
+                        36
+                      </Card>
+                    </Col>
+
+                    <Col span={8}>
+                      <Card title="review_mean(평가 평균)" bordered={false}>
+                        4.71
+                      </Card>
+                    </Col>
+
+                    <Col span={8}>
+                      <Card title="standard_deviation(표준편차)" bordered={false}>
+                        0.27
+                      </Card>
+                    </Col>
+
+                  </Row>
+                </div>
+          </Popularity>
+                        {/* <Row style={{padding : 60}}>
         <Col span={12}></Col>
         <Col span={12}>배종현</Col>
         </Row>
@@ -120,80 +154,51 @@ import styled from 'styled-components';
         <Col span={12}>이론학점 : 3</Col>
         <Col span={12}>실습학점 : 3</Col>
         </Row> */}
+                        <footer></footer>
+                    </Card>
+                </Wrap>
+            </YellowBack>
+        </>
+    );
+};
 
-          <footer>
-          <Divider orientation="left"><CommentOutlined />   리뷰 </Divider>
-          <Popularity>
-          <div className="site-card-wrapper">
-            <Row gutter={16}>
+const YellowBack = styled.div``;
 
-              <Col span={8}>
-                <Card title="review_number(수강평가한 인원)" bordered={false}>
-                  36
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="review_mean(평가 평균)" bordered={false}>
-                  4.71
-                </Card>
-              </Col>
-
-              <Col span={8}>
-                <Card title="standard_deviation(표준편차)" bordered={false}>
-                  0.27
-                </Card>
-              </Col>
-
-            </Row>
-          </div>
-          </Popularity>
-          </footer>
-          
-          </Card>
-        </Wrap>
-      </YellowBack>
-       
-      </>
-    )}
-  
-    const YellowBack = styled.div`
-        background-color : #fcd469;
-    `;    
-
-
-    const Wrap = styled.div`
+const Wrap = styled.div`
         position: relative;
-        margin-left : 200px;
-        margin-right : 200px;
+        margin: 20px;
         width : 75%
         display : flex;
         justify-content: center;
         align-items: center;
-    `;    
-
-    const Header = styled.div`
-        display : flex; 
     `;
 
-  
+const Header = styled.div`
+    display: flex;
+`;
 
-    const Popularity = styled.div`
-        margin-top : 30px; 
-        margin-bottom : 30px; 
-    `; 
+const Popularity = styled.div`
+    margin-top: 30px;
+    margin-bottom: 30px;
+`;
 
-    const HashTag = styled.div`
-        margin-top : 25px; 
-        margin-bottom : 25px; 
-        margin-left : 100px; 
-    `;  
-    
-    const Contents = styled.div`
-        margin-top : 30px; 
-    `;
+const HashTag = styled.div`
+    margin-top: 25px;
+    margin-bottom: 25px;
+    margin-left: 100px;
+`;
 
+const Contents = styled.div`
+    margin-top: 30px;
+    font-weight : 500;
+    margin-bottom: 30px;
+    font: 1.5rem;
+    line-height : 2.7em;
+`;
+
+const DividerWrap = styled.div `
+   font-size : 15px;
+   font weight : 600;
+`;
 
 export default Detail;
-
-
